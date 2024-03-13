9b8d7c275826a07764019da35eaac879e5052198
elmorao <miguelmora834@gmail.com>
2024-03-13 18:15:12 -0500

    first

---
+++ src/ImageComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import image1 from './assets/banda.png';
+import image2 from './assets/peaks.PNG';
+
+const ImageComponent = () => {
+  return (
+    <div className="image-container">
+      <div className="image-wrapper">
+        <img src={image1} alt="Image 1" />
+        <p>Nombre de la imagen 1</p>
+      </div>
+      <div className="image-wrapper">
+        <img src={image2} alt="Image 2" />
+        <p>Nombre de la imagen 2</p>
+      </div>
+    </div>
+  );
+}
+
+export default ImageComponent;
